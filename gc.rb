2b4b8b31fdd2ee87d0dadafd02bacee9e4f82b87
Phil Wilson <github@philwilson.org>
2013-09-24 13:52:54 +0100

    add notes and cache API body results to disk

--- gc.rb
+++ gc.rb
@@ -1,4 +1,5 @@
 require 'rubygems'
+require 'json'
 require 'sinatra'
 require 'awesome_print'
 require './gathercontent-api-client'
@@ -28,8 +29,16 @@ get '/project/:id' do
 end
 
 get '/page/:id' do
-	api = GatherContentApi.new('uniofbath', ENV['GATHERCONTENT_API_KEY'], 'x')
-	#@page = Hashie::Mash.new(api.get_page(params[:id]))
-	@page = api.get_page(params[:id])
+	page_filename = 'page-'+params[:id]
+	if File.exists?(page_filename)
+		puts "Loading from file"
+  		@page = Marshal.load File.read(page_filename)
+	else
+		puts "Loading from API"
+		api = GatherContentApi.new('uniofbath', ENV['GATHERCONTENT_API_KEY'], 'x')
+		@page = JSON.parse(api.get_page(params[:id]).body)
+		serialised_page = Marshal.dump(@page) # keep this line separate in case of Marshal errors
+		File.open('page-'+params[:id], 'w') {|f| f.write(serialised_page) }
+	end
 	erb :page
 end
