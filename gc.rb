a8f42ae4129520016188496479b6e59b6033817c
Phil Wilson <github@philwilson.org>
2013-10-02 22:48:09 +0100

    support clicking the button to re-fetch data from gathercontent

--- gc.rb
+++ gc.rb
@@ -4,9 +4,26 @@ require 'sinatra'
 require 'awesome_print'
 require './gathercontent-api-client'
 
+enable :sessions
+set :session_secret, 'super secret LOMG'
+
+
 helpers do
 	def custom_state_name(id)
 		"not defined yet" # fetch from disk or API if it doesn't exist
+	end
+end
+
+before do	
+	if request.query_string == 'refresh'
+		session[:refresh_from_api] = 'true'
+		redirect to(request.path)
+	end
+end
+
+after do
+	if response.status != 302 # don't reset before the 302 has completed or we won't know to refetch the data!
+		session[:refresh_from_api] = 'false'
 	end
 end
 
@@ -29,7 +46,7 @@ end
 get '/project/:id' do
 	project_pagelist_filename = 'project-pagelist/'+params[:id]
 
-	if File.exists?(project_pagelist_filename)
+	if session[:refresh_from_api] != 'true' && File.exists?(project_pagelist_filename)
 		puts "Loading from file"
   		@pagelist = Marshal.load(File.read(project_pagelist_filename))
 	else
@@ -43,16 +60,14 @@ get '/project/:id' do
 		File.open(project_pagelist_filename, 'w') {|f| f.write(serialised_pagelist) }
 	end
 
-
-	ap(@pagelist) # need to do this in an actual render!
-
 	erb :pagelist
 end
 
 get '/page/:id' do
+	puts session[:refresh_from_api].inspect
 	page_filename = 'pages/page-'+params[:id]
 
-	if File.exists?(page_filename)
+	if session[:refresh_from_api] != 'true' && File.exists?(page_filename)
 		puts "Loading from file"
   		@page = Marshal.load(File.read(page_filename))
 	else
